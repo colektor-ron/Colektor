3548f3a4c980d72dad2a875c40c1ce4f46b4efdc
Colektor <d.villegasronald@gmail.com>
2026-04-08 15:51:23 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-const CACHE='colektor-v51-nocache';
+const CACHE='colektor-v52';
 const ASSETS=['./index.html','./manifest.json','./icon.png','./logo.png'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting(); // Force immediate activation
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))) // Delete ALL caches
-  );
-  self.clients.claim();
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));});
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
