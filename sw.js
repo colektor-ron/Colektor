af9ab10923ed925cb081f95a51becf6528b86214
Colektor <d.villegasronald@gmail.com>
2026-04-13 17:09:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// ColeKtoR SW v2
+// ColeKtoR SW v1
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>caches.delete(n)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
