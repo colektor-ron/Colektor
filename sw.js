c41f8f53416e2c8faa4624981ce9f21603774250
Colektor <d.villegasronald@gmail.com>
2026-04-13 10:25:44 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-const CACHE='colektor-v63';
-const ASSETS=['./index.html','./manifest.json','./icon.png','./logo.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
-self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
+// v128-comics-musica
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>caches.delete(n)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
