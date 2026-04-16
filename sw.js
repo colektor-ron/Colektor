5b5ba98b75fcbe464882fd255f2d2c6ff531ee4c
Colektor <d.villegasronald@gmail.com>
2026-04-16 17:40:39 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='colektor-v45';
+const CACHE='colektor-v47';
 const ASSETS=['./index.html','./manifest.json','./icon.png','./logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
